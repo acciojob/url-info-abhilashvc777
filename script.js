085f6e53ba337afbb7c4540083def40c25f9763d
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-11 00:22:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let url = location.href
+// console.log(url)
+let count = 0;
+for(let i=0; i<=url.length-1; i++){
+	count++
+}
+alert(`The length of the URL is: ${count}`)
